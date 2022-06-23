4357bfbce33b717c4e0c3880ece9bbb281c7204b
Michał Kownacki <mk@michalkownacki.pl>
2022-06-23 10:48:49 +0200

    Refactor alphavantage.

--- src/utils/alphavantage.ts
+++ src/utils/alphavantage.ts
@@ -2,6 +2,11 @@ import { ALPHAVANTAGE_API_URL} from '../constants';
 import { Company, CompanyDetails } from '../types';
 import { OverviewResponseJson, SearchBestMatchesItem, SearchResponseJson, SearchResult, OverviewResult} from './alphavantageTypes';
 
+const createApiQueryUrl = (fun: 'SYMBOL_SEARCH' | 'OVERVIEW', query: string) => {
+  const queryArgName = fun === 'SYMBOL_SEARCH' ? 'keywords' : 'symbol';
+  return `${ALPHAVANTAGE_API_URL}query?function=${fun}&${queryArgName}=${query}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`;
+};
+
 const parseSearchBestMatchesItem = (bestMatchesItem: SearchBestMatchesItem): Company => {
   return {
     symbol: bestMatchesItem['1. symbol'],
@@ -19,11 +24,10 @@ const getCompaniesFromSearchResponse = async (response: Response): Promise<Compa
 };
 
 export const search = async (query: string): Promise<SearchResult> => {
-  // todo use env
-  const response = await fetch(`${ALPHAVANTAGE_API_URL}query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`);
+  const response = await fetch(createApiQueryUrl('SYMBOL_SEARCH', query));
   return response.status === 200
     ? {companies: await getCompaniesFromSearchResponse(response), isSuccess: true}
-    : {isSuccess: false}
+    : {isSuccess: false};
 };
 
 const parseOverviewResponseJson = (responseJson: OverviewResponseJson): CompanyDetails => {
@@ -40,9 +44,8 @@ const getDetailsFromOverviewResponse = async (response: Response): Promise<Compa
 };
 
 export const overview = async (symbol: string): Promise<OverviewResult> => {
-  // todo use env
-  const response = await fetch(`${ALPHAVANTAGE_API_URL}query?function=OVERVIEW&symbol=${symbol}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`);
+  const response = await fetch(createApiQueryUrl('OVERVIEW', symbol));
   return response.status === 200
     ? {details: await getDetailsFromOverviewResponse(response), isSuccess: true}
-    : {isSuccess: false}
+    : {isSuccess: false};
 };
