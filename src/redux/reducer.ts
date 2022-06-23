e100a9d5970d7c0e6e71b61e605b3cc754d273b8
Michał Kownacki <mk@michalkownacki.pl>
2022-06-23 13:51:44 +0200

    Move reducer to separete file.

---
+++ src/redux/reducer.ts
@@ -0,0 +1,67 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { Company } from '../types';
+import { ADD_TO_PORTFOLIO, REMOVE_FROM_PORTFOLIO, SET_QUERY, SET_SEARCH_RESULT } from './actionTypes';
+
+export interface StoreState {
+  query?: string,
+  searchResult: Company[],
+  portfolio: Company[],
+}
+
+const initialState: StoreState  = {
+  searchResult: [],
+  portfolio: [],
+};
+
+
+export const reducer = createReducer(
+  initialState,
+  {
+    [SET_QUERY]: (
+      state: StoreState,
+      resp: {
+        payload: string,
+      }
+    ) => {
+      return {
+        ...state,
+        query: resp.payload,
+      };
+    },
+    [SET_SEARCH_RESULT]: (
+      state: StoreState,
+      resp: {
+        payload: Company[],
+      }
+    ) => {
+      return {
+        ...state,
+        searchResult: resp.payload,
+      };
+    },
+    [ADD_TO_PORTFOLIO]: (
+      state: StoreState,
+      resp: {
+        payload: Company,
+      }
+    ) => {
+      return {
+        ...state,
+        portfolio: [...state.portfolio, resp.payload],
+      };
+    },
+    [REMOVE_FROM_PORTFOLIO]: (
+      state: StoreState,
+      resp: {
+        payload: { index: number },
+      }
+    ) => {
+      const newPortfolio = [...state.portfolio];
+      newPortfolio.splice(resp.payload.index, 1);
+      return {
+        ...state,
+        portfolio: newPortfolio,
+      };
+    },
+  },
+);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,76 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
-import { createReducer } from '@reduxjs/toolkit';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk'
-import { SET_QUERY, SET_SEARCH_RESULT, ADD_TO_PORTFOLIO, REMOVE_FROM_PORTFOLIO } from './actionTypes';
-import { Company } from '../types';
-
-export interface StoreState {
-  query?: string,
-  searchResult: Company[],
-  portfolio: Company[],
-}
-
-const initialState: StoreState  = {
-  searchResult: [],
-  portfolio: [],
-};
-
-export const appReducer = createReducer(
-  initialState,
-  {
-    [SET_QUERY]: (
-      state: StoreState,
-      resp: {
-        payload: string,
-      }
-    ) => {
-      return {
-        ...state,
-        query: resp.payload,
-      };
-    },
-    [SET_SEARCH_RESULT]: (
-      state: StoreState,
-      resp: {
-        payload: Company[],
-      }
-    ) => {
-      return {
-        ...state,
-        searchResult: resp.payload,
-      };
-    },
-    [ADD_TO_PORTFOLIO]: (
-      state: StoreState,
-      resp: {
-        payload: Company,
-      }
-    ) => {
-      return {
-        ...state,
-        portfolio: [...state.portfolio, resp.payload],
-      };
-    },
-    [REMOVE_FROM_PORTFOLIO]: (
-      state: StoreState,
-      resp: {
-        payload: { index: number },
-      }
-    ) => {
-      const newPortfolio = [...state.portfolio];
-      newPortfolio.splice(resp.payload.index, 1);
-      return {
-        ...state,
-        portfolio: newPortfolio,
-      };
-    },
-  },
-);
+import { reducer } from './reducer';
 
 // todo use configureStore
 export const store = createStore(
-  appReducer,
+  reducer,
   applyMiddleware(thunkMiddleware),
 );
 
-export type RootState = ReturnType<typeof appReducer>
+export type RootState = ReturnType<typeof reducer>
