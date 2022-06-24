9e699f4684ba89193f789ccc0fc86c5001e13585
Michał Kownacki <mk@michalkownacki.pl>
2022-06-24 05:40:11 +0200

    Add details tests.

---
+++ src/components/Details/Details.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { useParams } from 'react-router-dom';
+import { usePortfolio } from '../../redux/selectors';
+import { asMock } from '../../testutils/asMock';
+import { overview } from '../../utils/alphavantage';
+import { Details } from './Details';
+
+jest.mock('react-router-dom');
+jest.mock('../../utils/alphavantage');
+jest.mock('../../redux/selectors');
+
+describe('Details', () => {
+  it('should load details', async () => {
+    asMock(overview).mockResolvedValue({
+      details: {
+        address: 'ADDRESS_STUB',
+        description: 'DESC_STUB',
+        marketCapitalization: 'CAP_STUB',
+      },
+      isSuccess: true,
+    });
+
+    asMock(useParams).mockReturnValue({ companyIndex: '0' });
+    const companyMock = {name: 'NAME_STUB', symbol: 'SYMBOL_1'};
+    asMock(usePortfolio).mockReturnValue([companyMock]);
+
+    render(<Details />);
+
+    const name = await screen.findByText('NAME_STUB');
+    const address = await screen.findByText('ADDRESS_STUB');
+    const desc = await screen.findByText('DESC_STUB');
+    const cap = await screen.findByText('CAP_STUB');
+    expect(name).toBeInTheDocument();
+    expect(address).toBeInTheDocument();
+    expect(desc).toBeInTheDocument();
+    expect(cap).toBeInTheDocument();
+  });
+});
